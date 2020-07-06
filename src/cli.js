2e6ea16e5b1745f92ea4ae05f554a0e28e235e17
Ben Glynn <ben@benedictglynn.com>
2020-07-06 15:06:10 +0100

    Carry over TTY to spawn (#10)

--- src/cli.js
+++ src/cli.js
@@ -3,23 +3,10 @@ import ora from "ora";
 import errors from "./errors.json";
 import prepare from "./prepare";
 import { spawn } from "child_process";
-import inquirer from "inquirer";
 
 const parseArgs = (args) => {
   const parsed = arg({ "--ci": Boolean }, { argv: args.slice(2) });
   return { ci: parsed["--ci"] || false };
-};
-
-const getOptions = async (details) => {
-  const ciOptions = ["publish", `--tag ${details.headTag}`];
-  const askForOtp = () => {
-    const message = "What is your npm OTP?";
-    const prompt = { type: "number", name: "otp", message };
-    return inquirer.prompt([prompt]);
-  };
-  return details.ci
-    ? ciOptions
-    : askForOtp().then((answers) => ciOptions.concat(`--otp ${answers.otp}`));
 };
 
 const cli = (rawArgs) => {
@@ -29,12 +16,8 @@ const cli = (rawArgs) => {
     .then((details) => {
       const { packageName, headVersion, headTag } = details;
       spinner.succeed(`Publish ${packageName} v${headVersion} @${headTag}`);
-      return getOptions(details);
-    })
-    .then((options) => {
-      const publish = spawn("npm", options);
-      publish.stdout.pipe(process.stdout);
-      publish.stderr.pipe(process.stderr);
+      const args = ["publish", `--tag ${details.headTag}`];
+      spawn("npm", args, { stdio: [0, 1, 2] });
     })
     .catch((error) => {
       const message = errors[error.message] || error.toString();
