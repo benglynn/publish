018424d17ef692724b5524be41a6b1d5107cfaa2
Ben Glynn <ben@benedictglynn.com>
2020-07-08 17:01:56 +0100

    Fix spawn loop (#13)
    * Updated usage docs; publish script for this package.
    
    * 1.0.1-beta.0
    
    * 1.0.1
    
    * Report unexpected error
    
    * Less nested; report spawn close
    
    * 1.0.2-benglynn.0
    
    * Trying spawnSync to fix loop
    
    * 1.0.2-benglynn.1
    
    * remove stdio
    
    * 1.0.2-benglynn.2
    
    * Use different script name
    
    * 1.0.2-benglynn.3
    
    * remove close log
    
    * 1.0.2-benglynn.4

--- src/cli.js
+++ src/cli.js
@@ -4,19 +4,26 @@ import { spawn } from "child_process";
 import chalk from "chalk";
 
 const cli = async () => {
-  const spinner = ora("Preparing").start();
-  const { details, errors } = await prepare();
-  if (errors.length > 0) {
-    const message =
-      errors.length === 1
-        ? "There was a problem"
-        : `There were ${errors.length} problems`;
-    spinner.fail(chalk.bold(message));
-    errors.map((error) => console.log("-", chalk.cyan(error)));
-  } else {
-    spinner.succeed(`Publishing ${details.packageVersion} @${details.distTag}`);
-    const args = ["publish", `--tag=${details.distTag}`];
-    spawn("npm", args, { stdio: "inherit" });
+  try {
+    const spinner = ora("Preparing").start();
+    const { details, errors } = await prepare();
+    if (errors.length > 0) {
+      const message =
+        errors.length === 1
+          ? "There was a problem"
+          : `There were ${errors.length} problems`;
+      spinner.fail(chalk.bold(message));
+      errors.map((error) => console.log("-", chalk.cyan(error)));
+    } else {
+      spinner.succeed(
+        `Publishing ${details.packageVersion} @${details.distTag}`
+      );
+      const args = ["publish", `--tag=${details.distTag}`];
+      spawn("npm", args, { stdio: "inherit" });
+    }
+  } catch (e) {
+    spinner.fail("Oops! Something went wrong, sorry");
+    console.log(e);
   }
 };
 
