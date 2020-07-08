10172d7b6ebc33c6562e6df0f3ac14452b212859
Ben Glynn <ben@benedictglynn.com>
2020-07-08 16:15:09 +0100

    Report unexpected error

--- src/cli.js
+++ src/cli.js
@@ -5,19 +5,27 @@ import chalk from "chalk";
 
 const cli = async () => {
   const spinner = ora("Preparing").start();
-  const { details, errors } = await prepare();
-  if (errors.length > 0) {
-    const message =
-      errors.length === 1
-        ? "There was a problem"
-        : `There were ${errors.length} problems`;
-    spinner.fail(chalk.bold(message));
-    errors.map((error) => console.log("-", chalk.cyan(error)));
-  } else {
-    spinner.succeed(`Publishing ${details.packageVersion} @${details.distTag}`);
-    const args = ["publish", `--tag=${details.distTag}`];
-    spawn("npm", args, { stdio: "inherit" });
-  }
+  prepare()
+    .then(({ details, errors }) => {
+      if (errors.length > 0) {
+        const message =
+          errors.length === 1
+            ? "There was a problem"
+            : `There were ${errors.length} problems`;
+        spinner.fail(chalk.bold(message));
+        errors.map((error) => console.log("-", chalk.cyan(error)));
+      } else {
+        spinner.succeed(
+          `Publishing ${details.packageVersion} @${details.distTag}`
+        );
+        const args = ["publish", `--tag=${details.distTag}`];
+        spawn("npm", args, { stdio: "inherit" });
+      }
+    })
+    .catch((e) => {
+      spinner.fail("Oops! Something went wrong, sorry");
+      console.log(e);
+    });
 };
 
 export default cli;
