ef3d6a0e6c89549766fede61bdc72861a4d36357
Ben Glynn <ben@benedictglynn.com>
2020-07-08 16:23:24 +0100

    Less nested; report spawn close

--- src/cli.js
+++ src/cli.js
@@ -4,28 +4,28 @@ import { spawn } from "child_process";
 import chalk from "chalk";
 
 const cli = async () => {
-  const spinner = ora("Preparing").start();
-  prepare()
-    .then(({ details, errors }) => {
-      if (errors.length > 0) {
-        const message =
-          errors.length === 1
-            ? "There was a problem"
-            : `There were ${errors.length} problems`;
-        spinner.fail(chalk.bold(message));
-        errors.map((error) => console.log("-", chalk.cyan(error)));
-      } else {
-        spinner.succeed(
-          `Publishing ${details.packageVersion} @${details.distTag}`
-        );
-        const args = ["publish", `--tag=${details.distTag}`];
-        spawn("npm", args, { stdio: "inherit" });
-      }
-    })
-    .catch((e) => {
-      spinner.fail("Oops! Something went wrong, sorry");
-      console.log(e);
-    });
+  try {
+    const spinner = ora("Preparing").start();
+    const { details, errors } = await prepare();
+    if (errors.length > 0) {
+      const message =
+        errors.length === 1
+          ? "There was a problem"
+          : `There were ${errors.length} problems`;
+      spinner.fail(chalk.bold(message));
+      errors.map((error) => console.log("-", chalk.cyan(error)));
+    } else {
+      spinner.succeed(
+        `Publishing ${details.packageVersion} @${details.distTag}`
+      );
+      const args = ["publish", `--tag=${details.distTag}`];
+      const npm = spawn("npm", args, { stdio: "inherit" });
+      npm.on("close", (code) => console.log("closed with ${code}"));
+    }
+  } catch (e) {
+    spinner.fail("Oops! Something went wrong, sorry");
+    console.log(e);
+  }
 };
 
 export default cli;
